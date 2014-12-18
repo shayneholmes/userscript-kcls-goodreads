45cc831d23bffbf2c43750031e1eed8f8b146030
Shayne Holmes <simon.snth@xoxy.net>
2014-12-18 10:21:57 -0800

    Initial commit.

---
+++ userscript.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name       Add KCLS links to Goodreads
+// @namespace  http://use.i.E.your.homepage/
+// @version    1.01
+// @description  Puts links in Goodreads lists that go directly to a search at the King County Library System
+// @match      https://www.goodreads.com/review/list/*
+// @copyright  2014, Shayne Holmes
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
+// @require https://greasyfork.org/scripts/2199-waitforkeyelements/code/waitForKeyElements.js?version=6349
+// @author Shayne Holmes
+// ==/UserScript==
+
+$("th.title").after(function(){ return $(this).clone().removeClass('title').addClass('kcls').html('<div style="margin: auto 10px;">kcls</div>');});
+
+waitForKeyElements ("td.field.title", addkclslinks);
+
+function addkclslinks(jNode) { jNode.after(
+    function(){ return $(this).clone().removeClass('title').addClass('kcls')
+    .html("<div style='margin: auto 10px;'><a title='search on KCLS' href=http://kcls.bibliocommons.com/search?t=smart&search_category=keyword&q="
+    + encodeURIComponent(this.innerText) 
+    + "&commit=Search>kcls</a></div></td>");}
+); }
+
